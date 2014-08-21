4f2117a8a4fd8b37085d968d19a7b3ca5316fce4
Jijesh Mohan <jijeshmohan@gmail.com>
2014-08-21 22:27:07 +0530

    added json middleware

---
+++ middleware/json_middleware.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type JsonHandler struct {
+	Handler func(http.ResponseWriter, *http.Request) interface{}
+}
+
+func (t JsonHandler) marshal(item interface{}, w http.ResponseWriter) {
+	bytes, err := json.Marshal(item)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Write([]byte(")]}',")) // This is to avoid angularjs JSON Vulnerability Protection(https://docs.angularjs.org/api/ng/service/$http)
+	w.Write(bytes)
+}
+
+func (t JsonHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	data := t.Handler(w, r)
+	t.marshal(data, w)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	log.Println(r.URL, "-", r.Method, "-", r.RemoteAddr)
+}
--- server.go
+++ server.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	m "github.com/jijeshmohan/goseed/middleware"
 	"github.com/jijeshmohan/goseed/routes"
 )
 
@@ -18,27 +18,6 @@ func NewHttpServer(uiDir string) *HttpServer {
 	return &HttpServer{router: mux.NewRouter(), uiDir: uiDir}
 }
 
-type appHandler struct {
-	handler func(http.ResponseWriter, *http.Request) interface{}
-}
-
-func marshal(item interface{}, w http.ResponseWriter) {
-	bytes, err := json.Marshal(item)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	w.Write(bytes)
-}
-
-func (t appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	data := t.handler(w, r)
-	marshal(data, w)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	log.Println(r.URL, "-", r.Method, "-", r.RemoteAddr)
-}
-
 func (h *HttpServer) StartServer(address string) {
 	h.initRoutes()
 	log.Printf("Starting server at %s", address)
@@ -47,7 +26,7 @@ func (h *HttpServer) StartServer(address string) {
 
 func (h *HttpServer) initRoutes() {
 	for _, r := range routes.Routes {
-		h.router.Handle(r.Path, appHandler{r.F}).Methods(r.Method)
+		h.router.Handle(r.Path, m.JsonHandler{r.F}).Methods(r.Method)
 	}
 	h.router.PathPrefix("/").Handler(http.FileServer(http.Dir(h.uiDir)))
 }
