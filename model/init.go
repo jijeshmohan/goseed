773217cb5b3f9cf3ba837c72ac05a563372c9859
Jijesh Mohan <jijeshmohan@gmail.com>
2014-10-13 20:02:50 +0530

    Db should be initialize one and only once

--- model/init.go
+++ model/init.go
@@ -3,19 +3,26 @@ package model
 import (
 	// _ "github.com/go-sql-driver/mysql"
 
+	"log"
+	"sync"
+
 	"github.com/jinzhu/gorm"
 	// _ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var (
-	db gorm.DB
+	db   gorm.DB
+	once sync.Once
 )
 
 func InitDb() (err error) {
 	// db, err := gorm.Open("postgres", "user=gorm dbname=gorm sslmode=disable")
 	// db, err := gorm.Open("mysql", "gorm:gorm@/gorm?charset=utf8&parseTime=True")
-	db, err = gorm.Open("sqlite3", "./gorm.db")
-	db.AutoMigrate(User{})
+	once.Do(func() {
+		log.Println("Initialize database")
+		db, err = gorm.Open("sqlite3", "./gorm.db")
+		db.AutoMigrate(User{})
+	})
 	return
 }
